6c9d1e4d28687fd7e0173a275fb39d8195882cee
Akram Ghanname <akram@arlivesystems.com>
2021-06-14 16:29:16 +0100

    css challenge

--- css-challenge/src/App.js
+++ css-challenge/src/App.js
@@ -1,9 +1,208 @@
-import './App.css';
+import './App.scss';
 
 function App() {
+  const name = 'Jared';
   return (
     <div className="App">
-      <div className="Container">CODE GOES HERE</div>
+      <div className="container">
+        <div className="grid-main">
+          <h1>Hi {name}!</h1>
+          <span>23 Jan, 2021</span>
+          <div className="notification">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="26"
+              height="26"
+              fill="currentColor"
+              viewBox="0 0 16 16"
+            >
+              <path d="M8 16a2 2 0 0 0 2-2H6a2 2 0 0 0 2 2zm.995-14.901a1 1 0 1 0-1.99 0A5.002 5.002 0 0 0 3 6c0 1.098-.5 6-2 7h14c-1.5-1-2-5.902-2-7 0-2.42-1.72-4.44-4.005-4.901z" />
+            </svg>
+          </div>
+          <div className="search">
+            <div className="lens-icon">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                fill="currentColor"
+                className="bi bi-search"
+                viewBox="0 0 16 16"
+              >
+                <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
+              </svg>
+            </div>
+            <input
+              className="input-search"
+              type="text"
+              placeholder="Search"
+              name="search"
+            ></input>
+          </div>
+        </div>
+        <div className="grid-emoji">
+          <h3>How do you feel?</h3>
+          <div className="dots">
+            <span className="dot-white"></span>
+            <span className="dot-white"></span>
+          </div>
+          <div>
+            <span className="emoji"> &#128532;</span>
+            <p>Badly</p>
+          </div>
+          <div>
+            <span className="emoji"> &#128522;</span>
+            <p>Fine</p>
+          </div>
+          <div>
+            <span className="emoji"> &#128513;</span>
+            <p>Well</p>
+          </div>
+          <div>
+            <span className="emoji"> &#128515;</span>
+            <p>Excellent</p>
+          </div>
+        </div>
+
+        <div className="white-container">
+          <div className="flex-row">
+            <h2> Exercises </h2>
+            <div className="dots">
+              <span className="dot-black"></span>
+              <span className="dot-black"></span>
+            </div>
+          </div>
+
+          <div className="exercise-item">
+            <div className="flex-row">
+              <div className="heart">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="28"
+                  height="28"
+                  fill="currentColor"
+                  className="bi bi-heart-fill"
+                  viewBox="0 0 16 16"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z"
+                  />
+                </svg>
+              </div>
+              <div>
+                <h3>Speaking skills</h3>
+                <p>16 Exercises</p>
+              </div>
+            </div>
+
+            <div className="dots">
+              <span className="dot-black"></span>
+              <span className="dot-black"></span>
+            </div>
+          </div>
+          <div className="exercise-item">
+            <div className="flex-row">
+              <div className="user">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="30"
+                  height="30"
+                  fill="currentColor"
+                  className="bi bi-person-fill"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z" />
+                </svg>
+              </div>
+              <div>
+                <h3>Reading speed</h3>
+                <p>8 Exercises</p>
+              </div>
+            </div>
+
+            <div className="dots">
+              <span className="dot-black"></span>
+              <span className="dot-black"></span>
+            </div>
+          </div>
+          <div className="exercise-item">
+            <div className="flex-row">
+              <div className="heart">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="28"
+                  height="28"
+                  fill="currentColor"
+                  className="bi bi-heart-fill"
+                  viewBox="0 0 16 16"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314z"
+                  />
+                </svg>
+              </div>
+              <div>
+                <h3>Writing test</h3>
+                <p>14 Exercises</p>
+              </div>
+            </div>
+
+            <div className="dots">
+              <span className="dot-black"></span>
+              <span className="dot-black"></span>
+            </div>
+          </div>
+        </div>
+        <div className="navbar">
+          <nav>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="34"
+              height="34"
+              fill="currentColor"
+              className="bi bi-house-door-fill"
+              viewBox="0 0 16 16"
+            >
+              <path d="M6.5 14.5v-3.505c0-.245.25-.495.5-.495h2c.25 0 .5.25.5.5v3.5a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.146-.354L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.354 1.146a.5.5 0 0 0-.708 0l-6 6A.5.5 0 0 0 1.5 7.5v7a.5.5 0 0 0 .5.5h4a.5.5 0 0 0 .5-.5z" />
+            </svg>
+
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="34"
+              height="34"
+              fill="currentColor"
+              className="bi bi-grid-fill"
+              viewBox="0 0 16 16"
+            >
+              <path d="M1 2.5A1.5 1.5 0 0 1 2.5 1h3A1.5 1.5 0 0 1 7 2.5v3A1.5 1.5 0 0 1 5.5 7h-3A1.5 1.5 0 0 1 1 5.5v-3zm8 0A1.5 1.5 0 0 1 10.5 1h3A1.5 1.5 0 0 1 15 2.5v3A1.5 1.5 0 0 1 13.5 7h-3A1.5 1.5 0 0 1 9 5.5v-3zm-8 8A1.5 1.5 0 0 1 2.5 9h3A1.5 1.5 0 0 1 7 10.5v3A1.5 1.5 0 0 1 5.5 15h-3A1.5 1.5 0 0 1 1 13.5v-3zm8 0A1.5 1.5 0 0 1 10.5 9h3a1.5 1.5 0 0 1 1.5 1.5v3a1.5 1.5 0 0 1-1.5 1.5h-3A1.5 1.5 0 0 1 9 13.5v-3z" />
+            </svg>
+
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="34"
+              height="34"
+              fill="currentColor"
+              className="bi bi-envelope-fill"
+              viewBox="0 0 16 16"
+            >
+              <path d="M.05 3.555A2 2 0 0 1 2 2h12a2 2 0 0 1 1.95 1.555L8 8.414.05 3.555zM0 4.697v7.104l5.803-3.558L0 4.697zM6.761 8.83l-6.57 4.027A2 2 0 0 0 2 14h12a2 2 0 0 0 1.808-1.144l-6.57-4.027L8 9.586l-1.239-.757zm3.436-.586L16 11.801V4.697l-5.803 3.546z" />
+            </svg>
+
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="34"
+              height="34"
+              fill="currentColor"
+              className="bi bi-person-fill"
+              viewBox="0 0 16 16"
+            >
+              <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z" />
+            </svg>
+          </nav>
+        </div>
+      </div>
     </div>
   );
 }
