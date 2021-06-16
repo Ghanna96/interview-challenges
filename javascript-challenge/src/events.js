9dbea842ffb010342bd0235160a66cc2b6d7ee81
Akram Ghanname <akram@arlivesystems.com>
2021-06-16 13:13:53 +0100

    group by days

--- javascript-challenge/src/events.js
+++ javascript-challenge/src/events.js
@@ -23,17 +23,67 @@
       { id: 156, startsAt: '2021-01-27T17:01:11Z',  endsAt: '2021-01-27T22:01:11Z',  title: 'Dinner' },
     ],
     2: [
-      { id: 5676, startsAt: '2021-01-29T13:01:11Z',  endsAt: '2021-01-27T15:01:11Z',  title: 'Daily walk' },
+        { id: 5676, startsAt: '2021-01-29T13:01:11Z',  endsAt: '2021-01-29T15:01:11Z',  title: 'Daily walk' },
     ]
   }
  ```
 
  Your solution should not modify any of the function arguments
 */
+const { parseISO, differenceInDays } = require('date-fns');
+
+const eventsArray = [
+  {
+    id: 156,
+    startsAt: '2021-01-27T17:01:11Z',
+    endsAt: '2021-01-27T22:01:11Z',
+    title: 'Dinner',
+  },
+  {
+    id: 5676,
+    startsAt: '2021-01-29T13:01:11Z',
+    endsAt: '2021-01-31T15:01:11Z',
+    title: 'Daily walk',
+  },
+  {
+    id: 107,
+    startsAt: '2021-01-27T13:01:11Z',
+    endsAt: '2021-01-27T15:01:11Z',
+    title: 'Daily walk',
+  },
+];
 const groupEventsByDay = (events) => {
-  return events;
+  //argument validation
+  if (!events || events.length < 1) {
+    throw new Error('Invalid argument');
+  }
+  for (let i in events) {
+    if (typeof events[i] !== 'object') {
+      throw new Error('Events must contain only objects');
+    }
+  }
+
+  //sorting by date
+  const sortedEvents = events.sort((a, b) => {
+    return parseISO(a.startsAt) - parseISO(b.startsAt);
+  });
+  // console.log(sortedEvents);
+
+  //grouping by days
+  let firstDay = parseISO(sortedEvents[0].startsAt);
+  return sortedEvents.reduce((acc, event) => {
+    let diff = differenceInDays(parseISO(event.startsAt), firstDay);
+    if (acc.hasOwnProperty(diff)) {
+      acc[diff].push(event);
+      return acc;
+    } else {
+      acc[diff] = [event];
+      return acc;
+    }
+  }, {});
 };
 
+groupEventsByDay(eventsArray);
 /** 
   Adjust the start and end date of an event so it maintains its total duration, but is moved `toDay`.
   `eventsByDay` should be the same as the return value of `groupEventsByDay`
@@ -71,4 +121,9 @@ const groupEventsByDay = (events) => {
 */
 const moveEventToDay = (eventsByDay, id, toDay) => {
   return eventsByDay;
+};
+
+module.exports = {
+  moveEventToDay,
+  groupEventsByDay,
 };
--- javascript-challenge/src/events.test.js
+++ javascript-challenge/src/events.test.js
@@ -1,3 +1,84 @@
-test('groupEventsByDay', () => {});
+const { moveEventToDay, groupEventsByDay } = require('./events');
+
+const eventsArray = [
+  {
+    id: 107,
+    startsAt: '2021-01-27T13:01:11Z',
+    endsAt: '2021-01-27T15:01:11Z',
+    title: 'Daily walk',
+  },
+  {
+    id: 5676,
+    startsAt: '2021-01-30T13:01:11Z',
+    endsAt: '2021-01-30T15:01:11Z',
+    title: 'Daily walk',
+  },
+  {
+    id: 156,
+    startsAt: '2021-01-27T17:01:11Z',
+    endsAt: '2021-01-27T22:01:11Z',
+    title: 'Dinner',
+  },
+];
+
+const mockResult = {
+  0: [
+    {
+      id: 107,
+      startsAt: '2021-01-27T13:01:11Z',
+      endsAt: '2021-01-27T15:01:11Z',
+      title: 'Daily walk',
+    },
+    {
+      id: 156,
+      startsAt: '2021-01-27T17:01:11Z',
+      endsAt: '2021-01-27T22:01:11Z',
+      title: 'Dinner',
+    },
+  ],
+  3: [
+    {
+      id: 5676,
+      startsAt: '2021-01-30T13:01:11Z',
+      endsAt: '2021-01-30T15:01:11Z',
+      title: 'Daily walk',
+    },
+  ],
+};
+
+const invalidArray = [
+  {
+    id: 107,
+    startsAt: '2021-01-27T13:01:11Z',
+    endsAt: '2021-01-27T15:01:11Z',
+    title: 'Daily walk',
+  },
+  {
+    id: 156,
+    startsAt: '2021-01-27T17:01:11Z',
+    endsAt: '2021-01-27T22:01:11Z',
+    title: 'Dinner',
+  },
+  {
+    id: 5676,
+    startsAt: '2021-01-29T13:01:11Z',
+    endsAt: '2021-01-27T15:01:11Z',
+    title: 'Daily walk',
+  },
+  'hello',
+];
+describe('groupEventsByDay', () => {
+  it('throw error on invalid arguments', () => {
+    expect(() => groupEventsByDay()).toThrow('Invalid argument');
+    expect(() => groupEventsByDay([])).toThrow('Invalid argument');
+    expect(() => groupEventsByDay(invalidArray)).toThrow(
+      'Events must contain only objects',
+    );
+  });
+  it('group by days', () => {
+    expect(() => groupEventsByDay(eventsArray)).toBeDefined();
+    expect(groupEventsByDay(eventsArray)).toStrictEqual(mockResult);
+  });
+});
 
 test('moveEvent', () => {});
